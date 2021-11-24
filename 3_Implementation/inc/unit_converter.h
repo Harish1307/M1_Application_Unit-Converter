ab7df38e5fdf6f4343185688b289bf20fc111ace
MATRIXENCRYPT <kotichukkalapraneethsaikumar@gmail.com>
2021-11-24 18:48:03 +0530

    Push2

--- 3_Implementation/inc/unit_converter.h
+++ 3_Implementation/inc/unit_converter.h
@@ -1,8 +1,7 @@
 #ifndef _UNIT_CONVERTER_H_
 #define _UNIT_CONVERTER_H_
 
-#define <stdio.h>
-#define <sdtdlib.h>
+#include <stdio.h>
 
 int length_convert(int u1, int u2, int a);
 int mass_convert(int u1, int u2, int a);
